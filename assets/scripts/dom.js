c00d4f91f70acbe54c5fcadad1378c56c6440105
Your Name <you@example.com>
2023-02-03 20:03:20 +0000

    Cards and sidebar done

--- assets/scripts/dom.js
+++ assets/scripts/dom.js
@@ -1,21 +1,21 @@
-$(document). ready(() => {
+// $(document). ready(() => {
 
-    var Enter = $("#enter");
-    var Jumbo = $("#jumbo");
-    var Row1  = $("#row_1");
-    var Row2  = $("#row_2");
-    var Row3  = $("#row_3");
-    var Row4  = $("#row_4");
+//     var Enter = $("#enter");
+//     var Jumbo = $("#jumbo");
+//     var Row1  = $("#row_1");
+//     var Row2  = $("#row_2");
+//     var Row3  = $("#row_3");
+//     var Row4  = $("#row_4");
 
-    Row3.hide();
-    Row4.hide();
+//     Row3.hide();
+//     Row4.hide();
 
-    Enter.on('click', () => {
-    Jumbo.hide();
-    Row1.hide();
-    Row2.hide();
-    Row3.show();
-    Row4.show();
-})
+//     Enter.on('click', () => {
+//     Jumbo.hide();
+//     Row1.hide();
+//     Row2.hide();
+//     Row3.show();
+//     Row4.show();
+// })
 
-});
+// });
