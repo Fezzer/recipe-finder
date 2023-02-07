f85eb91c1e46de9b8d6e4004e3604cb9802e1e3d
Fezzer <chris.bowles@thescreamingsheep.net>
2023-02-07 18:38:32 +0000

    fetch instruction data and display recipe modal

--- assets/scripts/fetch-recipes.js
+++ assets/scripts/fetch-recipes.js
@@ -2,6 +2,7 @@
 const row4EL = $('#row_4');
 
 var apiKey = "37fe5fc3489647159a3bb09addfd0c09"
+var recipes = []; // Cache of the recipes returned by the API request.
 
 // This is where the Title and Image for the recipe will be printed
 const recipe = $('#api-test');
@@ -24,6 +25,9 @@ function getRecipes() {
         method: "GET"
 
     }).then(function(response) {
+
+    // Cache the response as this data will be needed when displaying the modal.
+    recipes = response;
 
     // Trigger HTML printing function - response is fed through
     displayRecipe(response);
@@ -54,7 +58,7 @@ function displayRecipe(api) {
 
         var cardButton = $('<a>')
             .attr("href", "#")
-            .attr("data-recipeId", api[i].id)
+            .attr("data-index", i)
             .addClass("btn btn-primary").text("View Recipe");
 
         // Append all HTML
